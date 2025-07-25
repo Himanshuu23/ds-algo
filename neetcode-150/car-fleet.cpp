081d3f66c366779767e4069ebc5c3a6e85abc1e9
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-07-25 16:53:43 +0530

    car fleet

--- neetcode-150/car-fleet.cpp
+++ neetcode-150/car-fleet.cpp
@@ -5,10 +5,20 @@
 using namespace std;
 typedef long long ll;
 
-// (target - pos) / speed -> steps to reach the target
+// (target - pos) / speed -> time to reach the target (distance / time)
 
 int solve(int t, vector<int>& p, vector<int>& s) {
-    
+    int n(p.size());
+    vector<pair<int, double>> v;
+    for (int i = 0; i < n; i++) v.push_back({p[i], (double)(t-p[i])/s[i]});
+    sort(v.rbegin(), v.rend());
+
+    stack<double> st;
+    for (auto& c : v) {
+        double time = c.second;
+        if (st.empty() || time > st.top()) st.push(time);
+    }
+    return st.size();
 }
 
 int main() {
