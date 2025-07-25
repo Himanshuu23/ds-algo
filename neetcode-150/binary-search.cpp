5ee60692c38a6b8ca64a6c447d23eb98ba554e2c
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-07-25 20:32:05 +0530

    binary search

---
+++ neetcode-150/binary-search.cpp
@@ -0,0 +1,34 @@
+/*
+    author: Himanshuu23
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int solve(vector<int>& v, int t) {
+    int l(0), r(v.size()-1);
+
+    while (l <= r) {
+        int mid = (l+r)/2;
+        if (v[mid] == t) return mid;
+        else if (v[mid] > t) r = mid - 1;
+        else l = mid + 1;
+    }
+
+    return -1;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long t; cin >> t;
+    while(t--) {
+        int n, t; cin >> n >> t;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) cin >> v[i];
+        cout << solve(v, t) << endl;
+    }
+
+    return 0;
+}
