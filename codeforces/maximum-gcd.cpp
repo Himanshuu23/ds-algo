cbb28f3d1392546a8b3b7e00fe0a6e6cbcb90db7
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-07-27 14:00:53 +0530

    1370 A

---
+++ codeforces/maximum-gcd.cpp
@@ -0,0 +1,19 @@
+/*
+    author: Himanshuu23
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        cout << n / 2 << '\n';
+    }
+
+    return 0;
+}
