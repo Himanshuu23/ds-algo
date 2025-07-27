fbfc916288c207822601f2d00debc37949bd6fed
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-07-27 12:18:24 +0530

    median of two sorted arrays

--- binary-search/median-two-sorted-array.cpp
+++ binary-search/median-two-sorted-array.cpp
@@ -11,13 +11,13 @@ double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int partitionX = (start + end) / 2;
         int partitionY = (m + n + 1) / 2 - partitionX; // cutting the nums1 in a way that left cut of nums1 gets the median and also all elements on left are less than the right ones
 
-        int maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1]; // last element on left side of nums1
-        int minRightX = (partitionX == m) ? INT_MAX : nums1[partitionX]; // first element on right side of nums1
+        int maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+        int minRightX = (partitionX == m) ? INT_MAX : nums1[partitionX];
 
-        int maxLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1]; // last element on left side of nums2
-        int minRightY = (partitionY == n) ? INT_MAX : nums2[partitionY]; // first element on right side of nums2
+        int maxLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+        int minRightY = (partitionY == n) ? INT_MAX : nums2[partitionY];
 
-        if (maxLeftX <= minRightY && maxLeftY <= minRightX) { // finding the correct size of right half for the partitioned nums1 by partitioning nums2 using partitionY - we want biggest on left <= smallest on right -> correct condition for calculation of median
+        if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
             if ((m + n) % 2 == 0)
                 return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0;
             else
