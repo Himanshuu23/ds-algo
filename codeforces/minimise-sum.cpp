54b7f72c3839fe07372da86b4179f12c0440aae5
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-07-29 13:45:02 +0530

    2124 B

---
+++ codeforces/minimise-sum.cpp
@@ -0,0 +1,21 @@
+/*
+    author: Himanshuu23
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) cin >> v[i];
+        cout << min(2*v[0], v[0]+v[1]) << endl;
+    }
+
+    return 0;
+}
