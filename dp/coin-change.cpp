5aea386848d2a149e680ae79821ca0a54eca8cf1
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-07-30 15:51:20 +0530

    using memoization

---
+++ dp/coin-change.cpp
@@ -0,0 +1,28 @@
+/*
+    author: Himanshuu23
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long t; cin >> t;
+    while(t--) {
+        int n, amt; cin >> n >> amt;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) cin >> v[i];
+        vector<int> dp(amt+1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 0; i <= amt; i++) {
+            for (int x : v) {
+                if (i-x>=0) dp[i] = min(dp[i], dp[i-x]+1);
+            }
+        }
+        cout << (dp[amt] == INT_MAX ? -1 : dp[amt]) << endl;
+    }
+
+    return 0;
+}
