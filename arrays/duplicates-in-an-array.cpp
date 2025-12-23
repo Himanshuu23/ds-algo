e52fcab5876b137f5e31d24092179d695efa7d6b
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-12-23 12:59:14 +0530

    Leetcode - 442

---
+++ arrays/duplicates-in-an-array.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> result;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int index = abs(nums[i])-1;
+            if (nums[index] < 0) result.push_back(index+1);
+            else nums[index] *= -1;
+        }
+
+        return result;
+    }
+};
