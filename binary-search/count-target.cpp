9af422e24eca549b6f69a00385ca18eee0d5f02f
Himanshu <sagarhimanshu472@gmail.com>
2026-01-11 11:37:20 +0530

    count target

--- binary-search/count-target.cpp
+++ binary-search/count-target.cpp
@@ -9,6 +9,33 @@ int solve(int n, vector<int>& v, int k) {
     return upper_bound(v.begin(), v.end(), k) - lower_bound(v.begin(), v.end(), k);
 }
 
+pair<int,int> firstAndLastOccurence(vector<int>&v, int n, int target) {
+	int first(-1), last(-1);
+	int start = 0, end = n-1;
+	while (start <= end) {
+		int middle = start + (end - start) / 2;
+		int value = v[middle];
+		if (value == target) {
+			first = middle;
+			end = middle - 1;
+		} else if (value < target) start = middle + 1;
+		else end = middle - 1;
+	}
+
+	start = 0, end = n-1;
+	while (start <= end) {
+		int middle = start + (end - start) / 2;
+		int value = v[middle];
+		if (value == target) {
+			last = middle;
+			start = middle + 1;
+		} else if (value < target) start = middle + 1;
+		else end = middle - 1;
+	}
+
+	return {first, last};
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -19,6 +46,9 @@ int main() {
         vector<int> v(n);
         for (int i = 0; i < n; i++) cin >> v[i];
         cout << solve(n, v, k) << endl;
+		auto {first, last} = firstAndLastOccurence(v, n, k);
+		if (first == -1) cout << -1 << endl;
+		else cout << last - first + 1 << endl;
     }
 
     return 0;
