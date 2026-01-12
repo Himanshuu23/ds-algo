b711d129d9f8c51a63a4e0db9553caaa6d0973d9
Himanshu <sagarhimanshu472@gmail.com>
2026-01-12 21:53:17 +0530

    Leetcode - 190

--- bit-manipulation/reverse-binary-bits.cpp
+++ bit-manipulation/reverse-binary-bits.cpp
@@ -6,12 +6,13 @@ using namespace std;
 typedef long long ll;
 
 int solve(int n) {
-    n = (n >> 16) | (n << 16);
-    n = (((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8));
-    n = (((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4));
-    n = (((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2));
-    n = (((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1));
-    return n;
+	int answer = 0;
+	for (int i = 0; i < 32; i++) {
+		answer = (answer << 1) | (1 & n);
+		n >>= 1;
+	}
+	
+	return answer;
 }
 
 int main() {
