06204fc38eb048e631d87716ddf750265aaf7981
Himanshu <sagarhimanshu472@gmail.com>
2026-02-19 22:57:44 +0530

    Leetcode - 16

--- arrays/three-sum-closest.cpp
+++ arrays/three-sum-closest.cpp
@@ -10,18 +10,17 @@ int solve(vector<int>& nums, int target) {
     for (int i = 0; i < n-2; i++) {
         if (i > 0 && nums[i] == nums[i-1]) continue;
         int l = i+1, r = n-1;
-        while (l < r) {
-            sum = nums[l] + nums[r] + nums[i];
-            if (abs(target - sum) < abs(target - answer)) {
-                answer = sum;
-                l++; r--;
-                while (l < r && nums[l] == nums[l-1]) l++;
-                while (l < r && nums[r] == nums[r+1]) r--;
-            }
-            else if (target > sum) r--;
-            else l++;
-        }
-    }
+       while (l < r) {
+    sum = nums[i] + nums[l] + nums[r];
+
+    if (sum == target) return target;
+
+    if (abs(target - sum) < abs(target - answer))
+        answer = sum;
+
+    if (sum > target) r--;
+    else l++;
+}    }
 
     return answer;
 }
