5e27f55dfb42cf6a558d58ceac67845de428d7f0
Himanshu <sagarhimanshu472@gmail.com>
2026-05-27 16:06:30 +0530

    feat: knapsack using tabulation

--- dp/knapsack.cpp
+++ dp/knapsack.cpp
@@ -1,32 +1,38 @@
-/*
-    author: Himanshuu23
-*/
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-
-int solve(int i, int W, vector<int>& wt, vector<int>& val, vector<vector<int>>& dp) {
-    if (i == wt.size()) return 0;
-    if (dp[i][W] != -1) return dp[i][W];
-
-    int exclude = solve(i+1,W,wt,val,dp);
-    int include(0);
-    if (wt[i] <= W) include = val[i] + solve(i+1,W-wt[i],wt,val,dp);
-    return dp[i][W] = max(include, exclude);
-}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    long t; cin >> t;
-    while(t--) {
+    int t; cin >> t;
+    while (t--) {
         int n, W; cin >> n >> W;
-        vector<int> wt(n), val(n);
-        for (int i = 0; i < n; i++) cin >> wt[i];
-        for (int i = 0; i < n; i++) cin >> val[i];
-        vector<vector<int>> dp(n+1, vector<int>(W + 1, -1));
-        cout << solve(0, W, wt, val, dp) << endl;
+        vector<int> v(n), w(n);
+
+        for (int i = 0; i < n; i++)
+            cin >> v[i];
+
+        for (int i = 0; i < n; i++)
+            cin >> w[i];
+
+        vector<vector<int>> dp(n+1, vector<int> (W+1));
+        for (int i = 0; i < n+1; i++)
+            dp[i][0] = 0;
+
+        for (int i = 0; i < W+1; i++)
+            dp[0][i] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= W; j++) {
+                int include = INT_MIN, exclude = INT_MIN;
+                exclude = dp[i-1][j];
+                if (j >= w[i-1]) include = v[i-1] + dp[i-1][j-w[i-1]];
+                dp[i][j] = max(include, exclude);
+            }
+        }
+
+        cout << dp[n][W] << '\n';
     }
 
     return 0;
