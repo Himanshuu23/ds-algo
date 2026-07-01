724081ce2a016f31bb24099e1a2f9d878fa492a2
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-07-01 13:32:08 +0530

    new solution using deque in O(n)

--- arrays/circular-subarray-maximum-sum.cpp
+++ arrays/circular-subarray-maximum-sum.cpp
@@ -1,6 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void usingDeque() {
+    int n; cin >> n;
+    vector<int> v(2*n);
+    for (int i = 0; i < n; i++) cin >> v[i];
+    for (int i = n; i < 2*n; i++) v[i] = v[i-n];
+
+    vector<long long> prefix(2*n);
+    prefix[0] = v[0];
+    for (int i = 1; i < 2*n; i++) prefix[i] = v[i] + prefix[i-1];
+
+    long long answer = INT_MIN;
+    deque<int> dq;
+    for (int i = 0; i < 2*n; i++) {
+        while (!dq.empty() && dq.front() < i - n) dq.pop_front();
+        if (!dq.empty())
+            answer = max(answer, prefix[i] - prefix[dq.front()]);
+        while (!dq.empty() && prefix[dq.back()] >= prefix[i]) dq.pop_back();
+        dq.push_back(i);
+    }
+
+    cout << answer << '\n';
+}
+
 int kadane(vector<int>& arr, int n) {
     int currSum = 0, maxSum = INT_MIN;
     for (int i = 0; i < n; i++) {
