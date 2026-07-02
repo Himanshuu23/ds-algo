f31791b2be10196393b818f6f913483edcbea1a3
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-07-02 10:56:51 +0530

    using dp and expand around center

--- strings/longest-palindromic-substring.cpp
+++ strings/longest-palindromic-substring.cpp
@@ -5,6 +5,25 @@
 using namespace std;
 typedef long long ll;
 
+void usingDP() {
+    string a, b; cin >> a;
+    int n = a.size();
+    b = a;
+    reverse(b.begin(), b.end());
+    vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+
+    string answer;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (a[i-1] == b[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+            int revStart = n - j;
+            if (dp[i][j] > answer.size() && revStart == i - dp[i][j]) answer = a.substr(i-dp[i][j], dp[i][j]);
+        }
+    }
+
+    cout << answer << '\n';
+}
+
 string solve(string s) {
     if (s.empty()) return "";
 
