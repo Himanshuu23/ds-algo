ef5b29df142c63fe54714895f6e3a79293bd2088
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-07-10 13:26:53 +0530

    climbing stairs

--- recursion/climbing-stairs.cpp
+++ recursion/climbing-stairs.cpp
@@ -1,6 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int solve(int n) {
+    if (n == 0 || n == 1) return n;
+    return solve(n-1) + solve(n-2);
+}
+
 class Solution { // O(2^n) solution
 public:
     void solve(int start, int end, int& answer) {
