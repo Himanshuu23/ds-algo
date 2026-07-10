bc0236910ad10b7ee2578e32ba128373305a0104
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-07-10 12:58:04 +0530

    removing duplicates

--- recursion/remove-duplicates.cpp
+++ recursion/remove-duplicates.cpp
@@ -5,6 +5,18 @@
 using namespace std;
 typedef long long ll;
 
+void solve(const string& s, unordered_map<char, int>& mp, int i) {
+    if (i == s.size()) {
+        cout << '\n';
+        return;
+    }
+    if (mp.find(s[i]) == mp.end()) {
+        cout << s[i];
+        mp[s[i]]++;
+    }
+    solve(s, mp, i+1);
+}
+
 string removeDup(string s, int i) {
     if (i == s.length()) return s;
     if (count(s.begin(), s.end(), s[i]) > 1) {
