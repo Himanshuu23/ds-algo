1bdddf30db9098e1f2bc7bd8e26c9b792ea0dae7
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-07-11 18:34:19 +0530

    generate all permutations

--- recursion/string-permutations.cpp
+++ recursion/string-permutations.cpp
@@ -5,6 +5,19 @@
 using namespace std;
 typedef long long ll;
 
+void solve(int i, string& s) {
+    if (i == s.size()) {
+        cout << s << '\n';
+        return;
+    }
+    for (int j = i; j < s.size(); j++) {
+        swap(s[i], s[j]);
+        solve(i+1, s);
+        swap(s[i], s[j]);
+    }
+}
+
+
 void printPermutations(string prefix, string remaining) {
     if (remaining.empty()) {
         cout << prefix << endl;
@@ -26,6 +39,7 @@ int main() {
     while(t--) {
         string s; cin >> s;
         printPermutations("", s);
+        // solve(0, s);
     }
 
     return 0;
