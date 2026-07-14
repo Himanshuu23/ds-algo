424edaf4748365c18cf51ca86e31444346c7cc4b
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-07-14 13:27:46 +0530

    connect nodes at same level

--- trees/connect-nodes-at-the-same-level.cpp
+++ trees/connect-nodes-at-the-same-level.cpp
@@ -28,8 +28,7 @@ void solve(Node* root) {
             Node* node = q.front(); q.pop();
             if (node->left) q.push(node->left);
             if (node->right) q.push(node->right);
-            level.push_back(node->left);
-            level.push_back(node->right);
+            level.push_back(node);
         }
 
         for (int i = 0; i < level.size() - 1; i++) level[i]->next = level[i+1];
