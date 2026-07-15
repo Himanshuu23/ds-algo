6d60a7341b327b630d15d762510450bd5397de35
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-07-16 05:25:59 +0530

    k frequent

--- heap-priority-queue/k-frequent.cpp
+++ heap-priority-queue/k-frequent.cpp
@@ -1,32 +1,29 @@
 /*
     author: Himanshuu23
 */
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
 using namespace std;
-typedef long long ll;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    long t; cin >> t;
-    while(t--) {
+    int T; cin >> T;
+    while (T--) {
         int n, k; cin >> n >> k;
         unordered_map<int, int> mp;
-        vector<tuple<int, int>> v;
+        priority_queue<pair<int, int>, vector<pair<int, int>>> pq;
         for (int i = 0; i < n; i++) {
-            int temp; cin >> temp;
-            mp[temp]++;
+            int x; cin >> x;
+            mp[x]++;
         }
-        for (const auto& [key, value] : mp) { v.emplace_back(value, key); }
-        make_heap(v.begin(), v.end());
+
+        for (auto& [key, value] : mp) pq.push({value, key});
         while (k--) {
-            pop_heap(v.begin(), v.end());
-            auto [ct, el] = v.back();
-            cout << el << " ";
-            v.pop_back();
+            cout << pq.top().second << " ";
+            pq.pop();
         }
-        cout << endl;
+        cout << '\n';
     }
 
     return 0;
