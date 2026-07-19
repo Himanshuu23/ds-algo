8c962752d76c1e4e29ac12d629286b9c41756fb3
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-07-19 19:08:17 +0530

    using prim's and kruskals

--- graphs/minimum-spanning-tree.cpp
+++ graphs/minimum-spanning-tree.cpp
@@ -10,32 +10,51 @@ typedef long long ll;
 // using Kruskal's Algo - sort edges by weight, pick smallest edge that doesn't form a cyle - Union-Find approach - O(ElogE)
 // if we apply Kruskal on non-connected graphs - then it returns spanning tree for each not connected component
 
-int find(int u, vector<int>& parent) {
-    return parent[u] == u ? u : parent[u] = find(parent[u], parent);
-}
+class DSU {
+	public:
+		vector<int> parent, rank_;
+		DSU (int n) {
+			parent.resize(n);
+			for (int i = 0; i < n; i++) parent[i] = i;
+			rank_.assign(n, 0);
+		}
 
-bool unionSets(int u, int v, vector<int>& parent) {
-    int pu = find(u, parent), pv = find(v, parent);
-    if (pu == pv) return false;
-    parent[pv] = pu;
-    return true;
-}
+		int find(int x) {
+			if (parent[x] != x) {
+				parent[x] = find(parent[x]);
+			}
+			return parent[x];
+		}
 
-int kruskal(int n, vector<tuple<int, int, int>>& edges) {
-    sort(edges.begin(), edges.end());
-    vector<int> parent(n);
-    iota(parent.begin(), parent.end(), 0);
-    int cost(0), count(0);
-    
-    for(auto& [w, u, v] : edges) {
-        if (unionSets(u, v, parent)) {
-            cost += w;
-            count++;
-            if (count == n - 1) break;
-        }
-    }
+		void unite(int x, int y) {
+			int rootX = find(x), rootY = find(y);
+			if (rootX == rootY) return;
+			if (rank_[rootX] < rank_[rootY]) swap(rootX, rootY);
+			parent[rootY] = rootX;
+			if (rank_[rootX] == rank_[rootY]) rank_[rootX]++;
+		}
 
-    return (count == n - 1) ? cost : -1;
+		bool hasCycle(int x, int y) {
+			int rootX = find(x), rootY = find(y);
+			if (rootX == rootY) return true;
+			unite(rootX, rootY);
+			return false;
+		}
+};
+
+int kruskal(int n, vector<tuple<int, int, int>> adj) {
+	int cost = 0, count = 0;
+	sort(adj.begin(), adj.end());
+	DSU dsu(n);
+	for (auto& [w, u, v] : adj) {
+		if (!dsu.hasCycle(u, v)) {
+			cost += w;
+			count++;
+			if (count == n - 1) break;
+		}
+	}
+
+	return (count == n - 1) ? cost : -1;
 }
 
 // using Prim's - start from any node, always pick the smallest edge that connects to unvisited node - min heap - O(ElogV)
