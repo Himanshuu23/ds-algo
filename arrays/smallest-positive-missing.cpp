8ac6796e6d046453cdb1c4044c784de727f734a2
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-07-28 23:11:52 +0530

    smallest missing positive

--- arrays/smallest-positive-missing.cpp
+++ arrays/smallest-positive-missing.cpp
@@ -1,6 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+
+        while (i < n) {
+            if (nums[i] <= 0 || nums[i] > n) {
+                i++;
+                continue;
+            }
+            int index = nums[i] - 1;
+            if (nums[i] != nums[index]) {
+                swap(nums[i], nums[index]);
+            } else {
+                i++;
+            }
+        }
+
+        for (i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+};
+
 const int N = 1e6 + 2;
 
 int smallestPositiveMissing(vector<int>& array, int n) {
