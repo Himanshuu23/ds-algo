9c7536be09413dff16e1941ae7646184fe7aea14
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-07-28 15:16:00 +0530

    designing hash set

---
+++ bit-manipulation/design-hashset.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+class MyHashSet {
+	private:
+		int set[31251];
+
+		int getMask(int key) {
+			return 1 << (key % 32);
+		}
+	
+	public:
+		MyHashSet() {
+			memset(set, 0, sizeof(set));
+		}
+
+		void add(int key) {
+			set[key/32] |= getMask(key);
+		}
+
+		void remove(int key) {
+			if (contains(key)) {
+				set[key/32] ^= getMask(key);
+			}
+		}
+
+		bool contains(int key) {
+			return (set[key/32] & getMask(key)) != 0;
+		}
+};
