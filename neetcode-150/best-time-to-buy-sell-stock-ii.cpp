7c1958daeadb86d5ecd87544d3eb578474bef4f4
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-07-28 21:42:49 +0530

    best time to buy and sell stock - ii

--- neetcode-150/best-time-to-buy-sell-stock-ii.cpp
+++ neetcode-150/best-time-to-buy-sell-stock-ii.cpp
@@ -18,3 +18,34 @@ int solve(int i, int bought, vector<int>& v, vector<vector<int>>& dp) {
 		return solve(0, 0, v, dp);
     }
 };
+
+// bottom up 
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][0] = max(dp[i + 1][0], -prices[i] + dp[i + 1][1]);
+            dp[i][1] = max(dp[i + 1][1], prices[i] + dp[i + 1][0]);
+        }
+
+        return dp[0][0];
+    }
+};
+
+// optimized bottom up - only next [i-1] required for current so O(1) space
+int solve(vector<int>& v) {
+	int nextBuy = 0, nextSell = 0;
+	int currentBuy = 0, currentSell = 0;
+	
+	for (int i = v.size() - 1; i >= 0; i--) {
+		currentBuy = max(currentBuy, nextSell - v[i]);
+		currentSell = max(currentSell, v[i] + nextBuy);
+		nextBuy = currentBuy;
+		nextSell = currentSell;
+	}
+
+	return currentBuy;
+}
