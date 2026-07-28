1cde29dae2ca087025b7b04a8a530127805eac45
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-07-29 00:03:34 +0530

    remove duplicates from sorted array

--- arrays/remove-duplicates-from-sorted-array.cpp
+++ arrays/remove-duplicates-from-sorted-array.cpp
@@ -4,6 +4,19 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+		int n = nums.size(), l = 0, r = 0;
+		while (r < n) {
+			nums[l] = nums[r];
+			while (r < n && nums[l] == nums[r]) r++;
+			l++;
+		}
+		return l;
+    }
+};
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
         int w(0), r(0), prev(-1e3);
 
         while (r < nums.size()) {
