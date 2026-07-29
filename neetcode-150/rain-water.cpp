26bbaa4fe407d9c29bb5e473f4b7b0c693d2242f
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-07-29 16:31:29 +0530

    trapping rainwater

--- neetcode-150/rain-water.cpp
+++ neetcode-150/rain-water.cpp
@@ -5,36 +5,24 @@
 using namespace std;
 typedef long long ll;
 
-int solve(int n, vector<int>& h) {
-    int l(0), r(n - 1), lm(0), rm(0), ans(0);
+class Solution {
+public:
+    int trap(vector<int>& height) {
+		int l = 0, r = height.size() - 1;
+		int leftMax = height[l], rightMax = height[r];
+		int answer = 0;
+		while (l < r) {
+			if (leftMax < rightMax) {
+				l++;
+				leftMax = max(leftMax, height[l]);
+				answer += leftMax - height[l];
+			} else {
+				r--;
+				rightMax = max(rightMax, height[r]);
+				answer += rightMax - height[r];
+			}
+		}
 
-    while (l <= r) {
-        if (h[l] <= h[r]) {
-            if (h[r] >= lm) lm = h[l];
-            else ans += lm - h[l];
-            l++;
-        } else {
-            if (h[r] >= rm) rm = h[r];
-            else ans += rm - h[r];
-            r--;
-        }
+		return answer;
     }
-    
-    return ans;
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    long t; cin >> t;
-    while(t--) {
-        int n; cin >> n;
-        vector<int> v(n);
-        for (int i = 0; i < n; i++) cin >> v[i];
-
-        cout << solve(n, v) << endl;
-    }
-
-    return 0;
-}
+};
