6ddf06e6f588eef839b79cc51320ae77a1340fbd
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-07-30 17:31:10 +0530

    median of two sorted arrays

--- binary-search/median-two-sorted-array.cpp
+++ binary-search/median-two-sorted-array.cpp
@@ -5,11 +5,11 @@ double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
     if (nums1.size() > nums2.size()) return findMedianSortedArrays(nums2, nums1);
 
     int m = nums1.size(), n = nums2.size();
-    int start = 0, end = m;
+    int start = 0, end = m; // how many elements we can give to left half from num1 -> either no element or all elements
 
     while (start <= end) {
-        int partitionX = (start + end) / 2;
-        int partitionY = (m + n + 1) / 2 - partitionX; // cutting the nums1 in a way that left cut of nums1 gets the median and also all elements on left are less than the right ones
+        int partitionX = (start + end) / 2; // how many elements go into left half from num1
+        int partitionY = (m + n + 1) / 2 - partitionX; // since half of elements would be (m + n) / 2 ( that + 1 is for odd case handling -> then remaining half after taking from num1 -> left half has num2 elements according to this
 
         int maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
         int minRightX = (partitionX == m) ? INT_MAX : nums1[partitionX];
