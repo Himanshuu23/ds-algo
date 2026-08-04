a740c9b3aa263679e47a283a9fbf2882c10521ab
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-08-04 10:56:49 +0530

    using monotonic stacks

--- stacks/largest-rectangle-histogram.cpp
+++ stacks/largest-rectangle-histogram.cpp
@@ -5,6 +5,27 @@
 using namespace std;
 typedef long long ll;
 
+// O(n2) time and O(1) space
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int answer = 0, n = heights.size();
+        for (int i = 0; i < n; i++) {
+            int height = heights[i];
+
+            int right = i + 1;
+            while (right < n && heights[right] >= height) right++;
+            int left = i - 1;
+            while (left >= 0 && heights[left] >= height) left--;
+            right--;
+            left++;
+            answer = max(answer, height*(right - left + 1));
+        }
+
+        return answer;
+    }
+};
+
 int solve(vector<int>& heights, int n) { // for each ith index finding all area from prev max than ith element till smaller than ith element but no including that in the window
     stack<int> st;
     int area(0);
