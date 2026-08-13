4d1f3bb3146c8295c1cb3466de6c94eace0b5813
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-08-13 13:51:48 +0530

    morris traversal inorder

--- trees/morris-inorder.cpp
+++ trees/morris-inorder.cpp
@@ -9,7 +9,7 @@ class Node {
         Node(int value) : value(value), left(nullptr), right(nullptr) {}
 };
 
-// O(N) inorder traversal in O(1) space complexity
+// O(N) inorder traversal in O(1) space complexity - Threaded Binary Tree (basically creating temporary threads to go back up the path to parent from child - no stack needed)
 void morrisInorder(Node* root) {
     Node* current = root;
 
@@ -18,17 +18,18 @@ void morrisInorder(Node* root) {
             cout << current->value << " ";
             current = current->right;
         } else {
+            // find inorder predecessor
             Node* previous = current->left;
             while (previous->right != nullptr && previous->right != current) {
                 previous = previous->right;
             }
 
             if (previous->right == nullptr) {
-                previous->right = current;
-                current = current->left;
+                previous->right = current; // create thread
+                current = current->left; // continue moving left
             } else {
                 // we have thread already
-                previous->right = nullptr;
+                previous->right = nullptr; // destroy thread
                 cout << current->value << " ";
                 current = current->right;
             }
