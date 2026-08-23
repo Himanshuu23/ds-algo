1a4c13fc8406a0c950e7a894188f96f12843d34f
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-08-23 10:05:28 +0530

    using bfs, dfs, inorder

--- trees/is-valid-BST.cpp
+++ trees/is-valid-BST.cpp
@@ -19,11 +19,42 @@ public:
 
 // other way -> inorder traversel of BST is a sorted array
 
+// dfs - O(n), O(h) - recursion stack
 bool isBST(Node* root, ll minVal, ll maxVal) {
     if (!root) return true;
     if (root->data <= minVal || root->data >= maxVal) return false;
     return isBST(root->left, minVal, root->data) && isBST(root->right, root->data, maxVal);
 }
+
+// bfs - O(n), O(h)
+class Solution {
+public:
+    bool isValidBST(Node* root) {
+        if (!root) return true;
+
+        queue<tuple<Node*, int, int>> q;
+        q.push({root, INT_MIN, INT_MAX});
+
+        while (!q.empty()) {
+            auto [node, left, right] = q.front();
+            q.pop();
+            
+            if (left >= node->data || right <= node->data) {
+                return false;
+            }
+
+            if (node->left) {
+                q.push({node->left, left, node->data});
+            }
+
+            if (node->right) {
+                q.push({node->right, node->data, right});
+            }
+        }
+
+        return true;
+    }
+};
 
 int main() {
     ios::sync_with_stdio(false);
