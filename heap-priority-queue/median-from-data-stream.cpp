85b1637c1c822ad51e9764202faa6d16a50873df
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-08-27 20:17:17 +0530

    using sorting and priority queue

--- heap-priority-queue/median-from-data-stream.cpp
+++ heap-priority-queue/median-from-data-stream.cpp
@@ -5,11 +5,35 @@
 using namespace std;
 typedef long long ll;
 
+// brute force: addNum() - O(m * nlogn), findMedian() - O(1), space - O(n) -> m is number of function calls
+class MedianFinder {
+    vector<int> v;
+public:
+    MedianFinder() {}
+    
+    void addNum(int num) {
+        v.push_back(num);
+        sort(v.begin(), v.end());
+    }
+    
+    double findMedian() {
+        int n = v.size();
+        if (n & 1) {
+            return (double)v[n/2];
+        } else {
+            return (v[(n-1)/2] + v[n/2]) / 2.0;
+        }
+    }
+};
+
+// maintaining minHeap -> stores right half of sorted array with smallest element on the top, maxheap - stores the left half of the sorted array and top is the largest in that. Now if n is odd then median is top of maxHeap else avarage of top of both hence O(logn), O(n)
+// Difference of total elements in each must be atmost 1
 class Stream{
     priority_queue<int> mxh;
     priority_queue<int, vector<int>, greater<int>> mnh;
 
 public:
+    // if maxHeap is not empty and incoming value is smaller than smallest element in maxHeap then insert into maxHeap. Otherwise insert into minHeap. Now rebalancing, if one heap becomes larger than the other by more than one, move the top element to other heap
     void addNum(int value) {
         if (mxh.empty() || value <= mxh.top()) mxh.push(value);
         else mnh.push(value);
@@ -23,6 +47,7 @@ public:
         }
     }
 
+    // if both heap have same number of elements - average of their tops, else top of heap with more elements
     double getMedian() {
         int mx = mxh.size(), mn = mnh.size();
         return ((mx + mn) % 2 == 0) ? (mxh.top() + mnh.top()) / 2.0 : (mx > mn ) ? mnh.top() : mxh.top();
