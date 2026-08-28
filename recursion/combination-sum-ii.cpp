316a91d5171eb2d90731df1e521de987a2919c16
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-08-28 10:57:29 +0530

    using backtracking and recursion

--- recursion/combination-sum-ii.cpp
+++ recursion/combination-sum-ii.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// recursion + backtracking - skipping duplicates so we generate only unique subsets. Also sorting early so if at some point while traversing 'i' value we have adding this element > target means we can't include that element in our current subset and all other elements after it can't be either so we return early instead of exploring them
-// O(n* 2^n)
+// brute force - O(n*2n) - exploring all possible subsets - gives TLE
 class Solution {
 private:
     void dfs(const vector<int>& nums, int target, int i, int sum, vector<int>& subset, vector<vector<int>>& result) {
@@ -13,7 +12,36 @@ private:
 
         for (int j = i; j < nums.size(); j++) {
             if (j > i && nums[j] == nums[j-1]) continue;
-            if (sum + nums[j] > target) {
+
+            subset.push_back(nums[j]);
+            dfs(nums, target, j + 1, sum + nums[j], subset, result);
+            subset.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<int> subset;
+        vector<vector<int>> result;
+        dfs(candidates, target, 0, 0, subset, result);
+        
+        return result;
+    }
+};
+
+// recursion + backtracking - skipping duplicates so we generate only unique subsets. Also sorting early so if at some point while traversing 'i' value we have adding this element > target means we can't include that element in our current subset and all other elements after it can't be either so we return early instead of exploring them -> (single optimization but massive improvement!)
+// O(n* 2^n)
+class Solution2 {
+private:
+    void dfs(const vector<int>& nums, int target, int i, int sum, vector<int>& subset, vector<vector<int>>& result) {
+        if (sum == target) {
+            result.push_back(subset);
+            return;
+        }
+
+        for (int j = i; j < nums.size(); j++) {
+            if (j > i && nums[j] == nums[j-1]) continue;
+            if (sum + nums[j] > target) { // optimization
                 return;
             }
 
