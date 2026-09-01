4f707ea397537a3e8bb102dc65ddb51b631a8504
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-09-01 12:21:39 +0530

    using backtracking and bit masking

--- recursion/n-queens.cpp
+++ recursion/n-queens.cpp
@@ -6,28 +6,6 @@ using namespace std;
 typedef long long ll;
 typedef vector<vector<string>> BoardList;
 
-void solve(int row, int n, vector<string>& board, vector<bool>& cols, 
-           vector<bool>& diag1, vector<bool>& diag2, BoardList& results) {
-    if (row == n) {
-        results.push_back(board);
-        return;
-    }
-
-    for (int col = 0; col < n; ++col) {
-        if (cols[col] || diag1[row + col] || diag2[row - col + n - 1])
-            continue;
-
-        board[row][col] = 'Q';
-        cols[col] = diag1[row + col] = diag2[row - col + n - 1] = true;
-
-        solve(row + 1, n, board, cols, diag1, diag2, results);
-
-        // backtrack
-        board[row][col] = '.';
-        cols[col] = diag1[row + col] = diag2[row - col + n - 1] = false;
-    }
-}
-
 // brute force - O(n! * n^2), O(n^2) - since for each row we have n choices to place the queens so overall n! and n^2 for checking if we can place
 bool canPlace(int i, int j, vector<vector<char>>& answer) {
     for (int row = 0; row < answer.size(); row++) {
@@ -60,6 +38,105 @@ void solve(int n, int i, vector<vector<char>>& answer) { // i -> current queens 
         }
     }
 }
+void solve(int row, int n, vector<string>& board, vector<bool>& cols, 
+           vector<bool>& diag1, vector<bool>& diag2, BoardList& results) {
+    if (row == n) {
+        results.push_back(board);
+        return;
+    }
+
+    for (int col = 0; col < n; ++col) {
+        if (cols[col] || diag1[row + col] || diag2[row - col + n - 1])
+            continue;
+
+        board[row][col] = 'Q';
+        cols[col] = diag1[row + col] = diag2[row - col + n - 1] = true;
+
+        solve(row + 1, n, board, cols, diag1, diag2, results);
+
+        // backtrack
+        board[row][col] = '.';
+        cols[col] = diag1[row + col] = diag2[row - col + n - 1] = false;
+    }
+}
+
+// same above solution O(n!) - we're not making those extra n^2 checks as below
+class Solution {
+private:
+    vector<bool> cols, diagonal1, diagonal2;
+    vector<vector<string>> result;
+
+    void solve(int row, int n, vector<string>& board) {
+        if (row == n) {
+            result.push_back(board);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (cols[col] || diagonal1[row + col] || diagonal2[row - col + n - 1]) continue; // diagonal2 is at row - col just it doesn't becomes negative we add n - 1 to it
+
+            board[row][col] = 'Q';
+            cols[col] = diagonal1[row + col] = diagonal2[row - col + n - 1] = true; 
+
+            solve(row + 1, n, board);
+
+            board[row][col] = '.';
+            cols[col] = diagonal1[row + col] = diagonal2[row - col + n - 1] = false; 
+        }
+    }
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        cols.resize(n, false);
+        diagonal1.resize(2*n-1, false);    
+        diagonal2.resize(2*n-1, false);
+        vector<string> board(n, string(n, '.'));  
+    
+        solve(0, n, board);
+
+        return result;
+    }
+};
+
+// same way as above - bit masking optimization - since integers would be faster than using maps, sets and less memory being used
+class Solution {
+private:
+    int column = 0, principal_diagonal = 0, off_diagonal = 0;
+    vector<string> board;
+    vector<vector<string>> result;
+
+    void backtrack(int row, int n) {
+        if (row == n) {
+            result.push_back(board);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if ((column & (1 << col)) || principal_diagonal & (1 << (row + col)) || (off_diagonal & (1 << (row - col + n)))) { // above off_diagonal was index of array that's why it was row - col + n - 1
+                continue;
+            }
+
+            column ^= (1 << col);
+            principal_diagonal ^= (1 << (row + col));
+            off_diagonal ^= (1 << (row - col + n));
+            board[row][col] = 'Q';
+
+            backtrack(row + 1, n);
+
+            column ^= (1 << col);
+            principal_diagonal ^= (1 << (row + col));
+            off_diagonal ^= (1 << (row - col + n));
+            board[row][col] = '.';
+        }
+    }
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        board.resize(n, string(n, '.'));
+
+        backtrack(0, n);
+
+        return result;    
+    }
+};
 
 int main() {
     int t;
