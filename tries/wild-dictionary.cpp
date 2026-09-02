d73da7cf405498aa66600baeaf6dd4f76cea75e5
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-09-02 13:01:45 +0530

    using tries

--- tries/wild-dictionary.cpp
+++ tries/wild-dictionary.cpp
@@ -3,30 +3,55 @@ using namespace std;
 
 class TrieNode {
     public:
-    unordered_map<char, TrieNode*> map;
+    unordered_map<char, TrieNode*> children;
     bool endOfWord = false;
 };
 
-class Trie {
-    private:
+class WordDictionary {
+public:
     TrieNode* root;
 
-    public:
-    Trie() {
+    WordDictionary() {
         root = new TrieNode();
     }
-
-    void insert(const string &word) {
+    
+    void addWord(string word) {
         TrieNode* current = root;
-        for (char character : word) {
-            if (current->map.find(character) == current->map.end())
-                current->map[character] = new TrieNode();
-            current = current->map[character];
+
+        for (char c : word) {
+            if (current->children.find(c) == current->children.end()) {
+                current->children[c] = new TrieNode();
+            }
+            current = current->children[c];
         }
         current->endOfWord = true;
     }
-};
 
-int main() {
-    return 0;
-}
+    bool search_helper(TrieNode* current, int i, string& word) {
+        if (i == word.size()) {
+            return current->endOfWord;
+        }
+
+        char c = word[i];
+
+        if (c != '.') {
+            if (current->children.find(c) == current->children.end()) {
+                return false;
+            }
+            return search_helper(current->children[c], i + 1, word); // move to next child
+        }
+
+        // current character is '.' -> we need to explore all the children and also we have to start in the remaining substring from current trie node and not from root
+        for (auto& [ch, child] : current->children) {
+            if (search_helper(child, i + 1, word)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+    bool search(string word) {
+        return search_helper(root, 0, word);
+    }
+};
