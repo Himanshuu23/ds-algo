7aebd86dbf42a6269180c0e9dfe9837a7a336303
Himanshuu23 <sagarhimanshu472@gmail.com>
2026-09-03 12:50:31 +0530

    using sorting and hash maps

---
+++ maps/verifying-an-alien-dictionary.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// using sorting - O(n * mlogn), O(m*n)
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        unordered_map<char, int> dictionary;
+
+        for (int i = 0; i < 26; i++) {
+            dictionary[order[i]] = i;
+        }
+
+        auto compare = [&](const string& a, const string& b) {
+            for (int i = 0; i < min(a.size(), b.size()); i++) {
+                if (a[i] != b[i]) {
+                    return dictionary[a[i]] < dictionary[b[i]];
+                }
+            }
+
+            return a.size() < b.size();
+        };
+
+        return is_sorted(words.begin(), words.end(), compare);
+    }
+};
+
+// O(m*n), O(1) where m -> number of words, n -> average length of each word
+class Solution2 {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        unordered_map<char, int> dictionary;
+
+        for (int i = 0; i < order.size(); i++) {
+            dictionary[order[i]] = i;
+        }
+
+        for (int i = 0; i < words.size() - 1; i++) {
+            string w1 = words[i], w2 = words[i+1];
+            int j = 0;
+
+            for (; j < w1.size(); j++) {
+                if (j == w2.size()) return false; // previous word has larger length than next word
+                if (w1[j] != w2[j]) {
+                    if (dictionary[w1[j]] > dictionary[w2[j]]) {
+                        return false;
+                    }
+                    break; // prev word smaller than next -> move to next pair
+                }
+            }
+        }
+
+        return true;
+    }
+};
